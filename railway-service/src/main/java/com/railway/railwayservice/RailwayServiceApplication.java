10e68276ac266b5b9194e9748c6a3e069bde9d72
Hansana Herath <subodhahansana@gmail.com>
2021-11-06 22:47:26 +0530

    initial

---
+++ railway-service/src/main/java/com/railway/railwayservice/RailwayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.railway.railwayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayServiceApplication.class, args);
+	}
+
+}
